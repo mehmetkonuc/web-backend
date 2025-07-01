de87f63095dc42ad373bd3319c48158bdabc6a0b
mehmetkonuc <mehmetkonuc@gmail.com>
2025-07-01 21:58:38 +0000

    deneme

--- apps/chat/apps.py
+++ apps/chat/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
-
+import os
 
 class ChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.chat'
+    # Explicit path setting
+    path = os.path.dirname(os.path.abspath(__file__))
